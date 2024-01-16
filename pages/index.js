1b3a8ba82d457172be16a47aa0da9b77f724cb35
wnsah814 <sjmskm@gmail.com>
2024-01-16 14:40:34 +0900

    add page title

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import PostsView from './PostView'
+import PostsView from "./PostView";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>HYTERIA</title>
+      </Head>
       <PostsView />
     </>
-  )
+  );
 }
